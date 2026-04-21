a255b22870b48ca4f1bd58adcd79c58449717438
Alina <a.puskis@gmail.com>
2026-04-21 21:04:19 +0200

    mejoras de código

---
+++ js/components.js
@@ -0,0 +1,15 @@
+// Función para cargar el footer
+function cargarComponente(archivo, idDestino, callback){
+    fetch(archivo)
+        .then(function(respuesta){
+            return respuesta.text();
+        })
+        .then(function(html){
+            document.getElementById(idDestino).innerHTML = html;
+
+            // Ejecuta la función callback despues de insertar el componente
+            if(callback) callback();
+        });
+}
+
+cargarComponente('/components/footer.html','mi-footer');
